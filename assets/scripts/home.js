1d686030141c745fbfc7b843ced8d7de0ddfe4e2
Axel <axel@ktbo.mx>
2016-01-21 17:56:01 -0600

    Setup plain html

---
+++ assets/scripts/home.js
@@ -0,0 +1,18 @@
+DEMO.home = (function(){
+    var _$body;
+
+
+    var _initVars = function _initVars(){
+        _$body = $('body');
+    };
+    var _initEvents = function _initEvents() {
+
+    };
+
+    return {
+        init: function init() {
+            _initVars();
+            _initEvents();
+        }
+    }
+})();
